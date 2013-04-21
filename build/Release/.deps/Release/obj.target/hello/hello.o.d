fa2a7459c8546c544acd6acb2fa626f5553a734d
bittu08 <bittu@bittu-Latitude-E5410.(none)>
2013-04-21 20:22:02 +0530

    Adding hello world addon for node js

---
+++ build/Release/.deps/Release/obj.target/hello/hello.o.d
@@ -0,0 +1,31 @@
+cmd_Release/obj.target/hello/hello.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/bittu/.node-gyp/0.8.20/src -I/home/bittu/.node-gyp/0.8.20/deps/uv/include -I/home/bittu/.node-gyp/0.8.20/deps/v8/include  -fPIC -Wall -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/hello/hello.o.d.raw  -c -o Release/obj.target/hello/hello.o ../hello.cc
+Release/obj.target/hello/hello.o: ../hello.cc \
+ /home/bittu/.node-gyp/0.8.20/src/node.h \
+ /home/bittu/.node-gyp/0.8.20/deps/uv/include/uv.h \
+ /home/bittu/.node-gyp/0.8.20/deps/uv/include/ares.h \
+ /home/bittu/.node-gyp/0.8.20/deps/uv/include/ares_version.h \
+ /home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/uv-unix.h \
+ /home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/ngx-queue.h \
+ /home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/ev.h \
+ /home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/eio.h \
+ /home/bittu/.node-gyp/0.8.20/deps/v8/include/v8.h \
+ /home/bittu/.node-gyp/0.8.20/deps/v8/include/v8stdint.h \
+ /home/bittu/.node-gyp/0.8.20/src/node_object_wrap.h \
+ /home/bittu/.node-gyp/0.8.20/src/node.h \
+ /home/bittu/.node-gyp/0.8.20/src/ev-emul.h \
+ /home/bittu/.node-gyp/0.8.20/src/eio-emul.h
+../hello.cc:
+/home/bittu/.node-gyp/0.8.20/src/node.h:
+/home/bittu/.node-gyp/0.8.20/deps/uv/include/uv.h:
+/home/bittu/.node-gyp/0.8.20/deps/uv/include/ares.h:
+/home/bittu/.node-gyp/0.8.20/deps/uv/include/ares_version.h:
+/home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/uv-unix.h:
+/home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/ngx-queue.h:
+/home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/ev.h:
+/home/bittu/.node-gyp/0.8.20/deps/uv/include/uv-private/eio.h:
+/home/bittu/.node-gyp/0.8.20/deps/v8/include/v8.h:
+/home/bittu/.node-gyp/0.8.20/deps/v8/include/v8stdint.h:
+/home/bittu/.node-gyp/0.8.20/src/node_object_wrap.h:
+/home/bittu/.node-gyp/0.8.20/src/node.h:
+/home/bittu/.node-gyp/0.8.20/src/ev-emul.h:
+/home/bittu/.node-gyp/0.8.20/src/eio-emul.h:
